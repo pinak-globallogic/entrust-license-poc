f88d5392e140c7532be721ad777d14a3133e7a68
Pawan Singh <pawansingh7407@gmail.com>
2022-07-04 10:29:51 +0530

    Card added on dashboard

--- src/Pages/Dashboard/CardWrapper.jsx
+++ src/Pages/Dashboard/CardWrapper.jsx
@@ -5,36 +5,62 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { styled } from "@mui/styles";
+import { Grid, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import KeyIcon from "@mui/icons-material/Key";
 
 const CustomBox = styled(Box)(() => ({
-  maxWidth: "300px",
+  maxWidth: "300px",  
 }));
 
 const CardWrapper = ({ data, onSubmitButton, ...props }) => {
   return (
-    <CustomBox sx={{}} {...props}>
-      <Card variant="outlined">
-        <CardContent sx={{ mb: 3 }}>
-          <Typography variant="h6" component="div" lineHeight={1.4}>
-            {data.title}
-          </Typography>
-          <Typography variant="caption" color="text.secondary">
-            {data.subtitle}
-          </Typography>
-          <Typography variant="body2" sx={{ mb: 3, mt: 3 }}>
-            {data.content.title}
-          </Typography>
-          <Typography variant="body2">{data.content.subtitle}</Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" onClick={onSubmitButton}>
-            <Typography variant="button" fontWeight={600} fontSize="small">
-              {data.action.buttonText}
+    <Grid item xs={4}>
+      <CustomBox sx={{}} {...props}>
+        <Card sx={{ height: 300 }} variant="outlined">
+          <CardContent sx={{ mb: 3, height: 200 }}>
+            <Grid xs={12} container>
+              <Grid xs={2}>
+                <IconButton aria-label="previous">
+                  {data.icon === "key" ? <KeyIcon /> : <SearchIcon />}
+                </IconButton>
+              </Grid>
+              <Grid xs={10}>
+                <Grid container xs={12}>
+                  <Grid xs={12}>
+                    <Typography variant="h6" component="div" lineHeight={1.4}>
+                      {data.title}
+                    </Typography>
+                  </Grid>
+                  <Grid xs={12}>
+                    <Typography variant="caption" color="text.secondary">
+                      {data.subtitle}
+                    </Typography>
+                  </Grid>
+                  <Grid xs={12}>
+                    <Typography variant="caption" color="text.secondary">
+                      {data.subtitle2}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Typography variant="body2" sx={{ mb: 3, mt: 3 }}>
+              {data.content.title}
             </Typography>
-          </Button>
-        </CardActions>
-      </Card>
-    </CustomBox>
+            <Typography variant="body2">{data.content.subtitle}</Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" onClick={onSubmitButton}>
+              <Typography variant="button" fontWeight={600} fontSize="small">
+                {data.action.buttonText}
+              </Typography>
+            </Button>
+          </CardActions>
+        </Card>
+      </CustomBox>
+    </Grid>
   );
 };
 
--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -8,8 +8,10 @@ import { LICENSE_SEARCH_PAGE_ROUTE } from "Routes";
 const cards = [
   {
     key: 1,
-    title: "Generate product key Mockup 2: Wizard",
+    icon: "key",
+    title: "Create product key",
     subtitle: "Order Entry",
+    subtitle2: "Application Defined Features",
     content: {
       title: "Order fulfillment from a line item.",
       subtitle:
@@ -21,8 +23,40 @@ const cards = [
   },
   {
     key: 2,
-    title: "License Database Search Mockup 3: Data Layout Rework",
-    subtitle: "",
+    icon: "key",
+    title: "Activate Key",
+    subtitle: "Activation",
+    subtitle2: "",
+    content: {
+      title: "Order fulfillment from a line item.",
+      subtitle: "Activate a key from an activation file or manually.",
+    },
+    action: {
+      buttonText: "ACTIVATE KEY",
+    },
+  },
+  {
+    key: 3,
+    icon: "key",
+    title: "Generate product key",
+    subtitle: "Deactivated Card",
+    subtitle2: "",
+    content: {
+      title:
+        "This item cannot be accessed with the currently assumed role. Contact the administrator for questions.",
+      subtitle: "",
+    },
+    action: {
+      buttonText: "GENERATE NEW PRODUCT KEY",
+    },
+  },
+
+  {
+    key: 4,
+    icon: "search",
+    title: "Search",
+    subtitle: "License Explorer",
+    subtitle2: "",
     content: {
       title: "Order fulfillment from a line item.",
       subtitle:
@@ -30,6 +64,35 @@ const cards = [
     },
     action: {
       buttonText: "EXPLORE THE DATABASE",
+    },
+  },
+  {
+    key: 5,
+    icon: "search",
+    title: "Modify product key",
+    subtitle: "Rehosting",
+    subtitle2: "",
+    content: {
+      title: "Order fulfillment from a line item.",
+      subtitle:
+        "You can create multiple keys using the same settings here, as well.",
+    },
+    action: {
+      buttonText: "EXPLORE THE DATABASE",
+    },
+  },
+  {
+    key: 6,
+    icon: "key",
+    title: "Admin Panel",
+    subtitle: "",
+    subtitle2: "",
+    content: {
+      title: "Manage roles and application defined feature settings.",
+      subtitle: "",
+    },
+    action: {
+      buttonText: "GO TO ADMIN PANEL",
     },
   },
 ];
@@ -41,8 +104,56 @@ const Dashboard = (props) => {
     navigate("/license/generate");
   };
 
+  const onActivateKey = () => {
+    navigate("/");
+  };
+
+  const onModifyProductKey = () => {
+    navigate("/");
+  };
+
+  const onAdminPanel = () => {
+    navigate("/");
+  };
+
+  const onCreateProductKey = () => {
+    navigate("/");
+  };
+
+  const goToDashBord = () => {
+    navigate("/");
+  };
+
   const onSearchProductKey = () => {
     navigate(LICENSE_SEARCH_PAGE_ROUTE);
+  };
+
+  const getMethodName = (card) => {
+    var methodName = goToDashBord;
+    switch (card.key) {
+    case 1:
+      methodName = onGenerateProductKeyClick;
+      break;
+    case 2:
+      methodName = onActivateKey;
+      break;
+    case 3:
+      methodName = onCreateProductKey;
+      break;
+    case 4:
+      methodName = onSearchProductKey;
+      break;
+    case 5:
+      methodName = onModifyProductKey;
+      break;
+    case 6:
+      methodName = onAdminPanel;
+      break;
+    default:
+      methodName = goToDashBord;
+      break;
+    }
+    return methodName;
   };
 
   return (
@@ -52,21 +163,25 @@ const Dashboard = (props) => {
           <Typography variant="h4">Dashboard</Typography>
         </Grid>
         <Grid item mb={6}>
-          <Typography variant="caption">
-            Welcome to the Internal Licensing Tool. This is your Dashboard, from
-            where you can access all available functions. Below the header of
-            each card, the legacy name of the function in regard to the old
-            toolset is mentioned.
-          </Typography>
+          <Grid xs>
+            <Typography variant="caption">
+              Welcome to the Internal Licensing Tool. This is your Dashboard,
+              from where you can access all available functions.
+            </Typography>
+          </Grid>
+          <Grid xs>
+            <Typography variant="caption">
+              Below the header of each card, the legacy name of the function in
+              regard to the old toolset is mentioned.
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid item>
+        <Grid container xs={12}>
           {cards.map((card) => (
             <CardWrapper
               key={card.key}
               mb={5}
-              onSubmitButton={
-                card.key === 2 ? onSearchProductKey : onGenerateProductKeyClick
-              }
+              onSubmitButton={getMethodName(card)}
               data={card}
             />
           ))}
