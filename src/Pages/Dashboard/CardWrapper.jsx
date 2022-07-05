886aa18c4826f3fb4815148694f708216d520e74
Pawan Singh <pawansingh7407@gmail.com>
2022-07-05 15:48:48 +0530

    added useStyle

--- src/Pages/Dashboard/CardWrapper.jsx
+++ src/Pages/Dashboard/CardWrapper.jsx
@@ -4,7 +4,7 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { styled } from "@mui/styles";
+import { makeStyles, styled } from "@mui/styles";
 import { Grid, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import KeyIcon from "@mui/icons-material/Key";
@@ -13,50 +13,61 @@ const CustomBox = styled(Box)(() => ({
   maxWidth: "300px",
 }));
 
+const useStyles = makeStyles({
+  card: {
+    height: 300,
+  },
+  content: { mb: 3, height: 200 },
+  disableCard: { opacity: 0.5, pointerEvents: "none" },
+});
+
 const CardWrapper = ({ data, onSubmitButton, ...props }) => {
+  const classes = useStyles();
   return (
-    <Grid
-      item
-      xs={4}
-      sx={!data.enable ? { opacity: 0.5, pointerEvents: "none" } : {}}
-    >
-      <CustomBox sx={{}} {...props}>
-        <Card sx={{ height: 300 }} variant="outlined">
-          <CardContent sx={{ mb: 3, height: 200 }}>
+    <Grid item xs={4} className={!data.enable ? classes.disableCard : {}}>
+      <CustomBox {...props}>
+        <Card className={classes.card} variant="outlined">
+          <CardContent className={classes.content} id={"cardContent-"+ data.key}>
             <Grid xs={12} container>
               <Grid xs={2}>
-                <IconButton aria-label="previous">
+                <IconButton aria-label="previous" id={"titleIcon-"+ data.key}>
                   {data.icon === "key" ? <KeyIcon /> : <SearchIcon />}
                 </IconButton>
               </Grid>
               <Grid xs={10}>
                 <Grid container xs={12}>
                   <Grid xs={12}>
-                    <Typography variant="h6" component="div" lineHeight={1.4}>
+                    <Typography variant="h6" component="div" lineHeight={1.4} id={"title-"+ data.key}>
                       {data.title}
                     </Typography>
                   </Grid>
                   <Grid xs={12}>
-                    <Typography variant="caption" color="text.secondary">
+                    <Typography variant="caption" color="text.secondary" id={"secondaryTitle-"+ data.key}>
                       {data.subtitle}
                     </Typography>
                   </Grid>
                   <Grid xs={12}>
-                    <Typography variant="caption" color="text.secondary">
+                    <Typography variant="caption" color="text.secondary" id={"secondary2Title-"+ data.key}>
                       {data.subtitle2}
                     </Typography>
                   </Grid>
                 </Grid>
               </Grid>
             </Grid>
-            <Typography variant="body2" sx={{ mb: 3, mt: 3 }}>
+            <Typography variant="body2" sx={{ mb: 3, mt: 3 }} id={"contentTitle-"+ data.key}>
               {data.content.title}
             </Typography>
-            <Typography variant="body2">{data.content.subtitle}</Typography>
+            <Typography variant="body2"  id={"contentSubTitle-"+ data.key}>{data.content.subtitle}</Typography>
           </CardContent>
           <CardActions>
             <Button size="small" onClick={onSubmitButton}>
-              <Typography variant="button" fontWeight={600} fontSize="small" color={data.enable ? "" : "gray"}>
+              <Typography
+                variant="button"
+                fontWeight={600}
+                fontSize="small"
+                color={data.enable ? "" : "gray"}
+                id={"actionOfCard-"+ data.key}
+              >
                 {data.action.buttonText}
               </Typography>
             </Button>
