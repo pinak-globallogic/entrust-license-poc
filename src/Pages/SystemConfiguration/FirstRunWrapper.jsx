db93e9cd9a40c98bb887c922f83c35bb4c1c30fc
pinak-globallogic <pinak.1@globallogic.com>
2022-07-06 16:25:35 +0530

    Remove appbar from system config component

--- src/Pages/SystemConfiguration/FirstRunWrapper.jsx
+++ src/Pages/SystemConfiguration/FirstRunWrapper.jsx
@@ -1,25 +1,9 @@
 import FirstRunStepper from "./FirstRunStepper";
 import Grid from "@mui/material/Grid";
-import ResponsiveAppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Container from "@mui/material/Container";
-import logo from "Assets/Images/entrust-logo.png";
 
 const FirstRunWrapper = () => {
   return (
     <>
-      <ResponsiveAppBar>
-        <Container>
-          <Toolbar disableGutters>
-            <img
-              src={logo}
-              loading="lazy"
-              alt="entrust-logo.png"
-              style={{ height: "40px" }}
-            />
-          </Toolbar>
-        </Container>
-      </ResponsiveAppBar>
       <Grid container direction="row" alignItems="center" pt={4}>
         <FirstRunStepper />
       </Grid>
