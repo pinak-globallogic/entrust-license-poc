fafaa060911a75f5e1ce039cac8b56e00bfd9734
Sakshi Gupta <sakshi.gupta3@globallogic.com>
2022-07-11 15:04:03 +0530

    infoIcon toggle changes

--- src/Pages/License/Activate/DetailKeyInfo.jsx
+++ src/Pages/License/Activate/DetailKeyInfo.jsx
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 import {
   Box,
   Button,
-  ClickAwayListener,
   Typography,
   Tab,
   Tabs,
@@ -65,7 +64,7 @@ function a11yProps(index) {
   };
 }
 
-const DetailKeyInfo = ({isInfoOpen, setInfoOpen, keyID}) => {
+const DetailKeyInfo = ({ isInfoOpen, setInfoOpen, keyID }) => {
   const [value, setValue] = React.useState(2);
   console.log(keyID);
 
@@ -77,89 +76,83 @@ const DetailKeyInfo = ({isInfoOpen, setInfoOpen, keyID}) => {
     setInfoOpen((prev) => !prev);
   };
 
-  const handleClickAway = () => {
-    setInfoOpen(false);
-  };
-
   return (
-    <ClickAwayListener onClickAway={handleClickAway}>
-      <CustomCard>
-        <CustomCardContent style={{ backgroundColor: "#fff" }} sx={{ mt: 2 }}>
-          <Button
-            variant="text"
-            onClick={handleClick}
-            startIcon={<ArrowDropDownIcon />}
-            style={{
-              color: "black",
-              justifyContent: "flex-start",
-              textTransform: "none",
-            }}
-          >
-            <Typography variant="h6">Detail Product Key Information</Typography>
-          </Button>
-          {isInfoOpen ? (
-            <Box sx={{ width: "100%" }}>
-              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                <Tabs
-                  value={value}
-                  onChange={handleChange}
-                  variant="scrollable"
-                  scrollButtons="auto"
-                  aria-label="scrollable auto tabs example"
-                >
-                  <Tab label="DETAILS" {...a11yProps(0)} />
-                  <Tab label="CONTACT INFORMATION" {...a11yProps(1)} />
-                  <Tab label="APPLICATION DEFINED FEATURES" {...a11yProps(2)} />
-                  <Tab label="ACTIVITY LOG" {...a11yProps(3)} />
-                </Tabs>
-              </Box>
-              <TabPanel value={value} index={0}>
-                DETAILS
-              </TabPanel>
-              <TabPanel value={value} index={1}>
-                CONTACT INFORMATION
-              </TabPanel>
-              <TabPanel value={value} index={2}>
-                <TableContainer component={Paper}>
-                  <Table
-                    sx={{ minWidth: 100 }}
-                    aria-label="simple table"
-                    size="small"
-                  >
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>Fieldname</TableCell>
-                        <TableCell align="right">Int Value</TableCell>
-                        <TableCell align="right">String val</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {rows.map((row) => (
-                        <TableRow
-                          key={row.name}
-                          sx={{
-                            "&:last-child td, &:last-child th": { border: 0 },
-                          }}
-                        >
-                          <TableCell component="th" scope="row">
-                            {row.fieldName}
-                          </TableCell>
-                          <TableCell align="right">{row.intVal}</TableCell>
-                          <TableCell align="right">{row.stringVal}</TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-              </TabPanel>
-              <TabPanel value={value} index={3}>
-                ACTIVITY LOG
-              </TabPanel>
+    <CustomCard>
+      <CustomCardContent style={{ backgroundColor: "#fff" }} sx={{ mt: 2 }}>
+        <Button
+          variant="text"
+          onClick={handleClick}
+          startIcon={<ArrowDropDownIcon />}
+          style={{
+            color: "black",
+            justifyContent: "flex-start",
+            textTransform: "none",
+          }}
+        >
+          <Typography variant="h6">Detail Product Key Information</Typography>
+        </Button>
+        {isInfoOpen ? (
+          <Box sx={{ width: "100%" }}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                variant="scrollable"
+                scrollButtons="auto"
+                aria-label="scrollable auto tabs example"
+              >
+                <Tab label="DETAILS" {...a11yProps(0)} />
+                <Tab label="CONTACT INFORMATION" {...a11yProps(1)} />
+                <Tab label="APPLICATION DEFINED FEATURES" {...a11yProps(2)} />
+                <Tab label="ACTIVITY LOG" {...a11yProps(3)} />
+              </Tabs>
             </Box>
-          ) : null}
-        </CustomCardContent>
-      </CustomCard>
-    </ClickAwayListener>
+            <TabPanel value={value} index={0}>
+              DETAILS
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              CONTACT INFORMATION
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+              <TableContainer component={Paper}>
+                <Table
+                  sx={{ minWidth: 100 }}
+                  aria-label="simple table"
+                  size="small"
+                >
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Fieldname</TableCell>
+                      <TableCell align="right">Int Value</TableCell>
+                      <TableCell align="right">String val</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row) => (
+                      <TableRow
+                        key={row.fieldName}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell component="th" scope="row">
+                          {row.fieldName}
+                        </TableCell>
+                        <TableCell align="right">{row.intVal}</TableCell>
+                        <TableCell align="right">{row.stringVal}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+              ACTIVITY LOG
+            </TabPanel>
+          </Box>
+        ) : null}
+      </CustomCardContent>
+    </CustomCard>
   );
 };
 
